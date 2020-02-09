19ab74df553b0554e2c882ab126ffab585753f52
duylong <duylong3110@gmail.com>
2020-02-09 19:06:50 +0700

    fix comment PR

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,17 +8,6 @@ use ViralsInfyom\ViralsPermission\Controllers\Controller;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-//        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->middleware('verified');
