4d01bb678077f39c5895480e0aa2893538faad68
duylong <duylong3110@gmail.com>
2020-02-13 11:25:03 +0700

    unit test

---
+++ packages/ViralsPackage/ViralsInventory/tests/BaseTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace ViralsPackage\ViralsInventory\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class BaseTest extends BaseTestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
